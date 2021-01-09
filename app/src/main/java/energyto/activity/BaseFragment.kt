986854446437f42251ee23b357ad50fef13b336e
mahdi darvishi <mahdidrv78@gmail.com>
2021-01-09 05:23:49 +0330

    create base fragment

---
+++ app/src/main/java/energyto/activity/BaseFragment.kt
@@ -0,0 +1,23 @@
+package energyto.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IntegerRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    abstract var layoutRes: Int
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layoutRes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViews()
+    }
+
+    abstract fun setViews()
+}

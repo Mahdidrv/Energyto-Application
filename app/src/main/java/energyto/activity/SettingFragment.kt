9ab5cf804356dce8f94dc36f461460a060fe0cc8
mahdi darvishi <mahdidrv78@gmail.com>
2021-01-09 05:24:28 +0330

    create viewPager fragment

---
+++ app/src/main/java/energyto/activity/SettingFragment.kt
@@ -0,0 +1,12 @@
+package energyto.activity
+
+import ir.mahdidrv.energyto.R
+
+class SettingFragment: BaseFragment() {
+
+    override var layoutRes: Int = R.layout.notif_layout
+
+    override fun setViews() {
+        TODO("Not yet implemented")
+    }
+}
